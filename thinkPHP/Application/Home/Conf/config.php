2f963b4d1f383cbf8039f64ef4047a5540fd7111
githubtown <1020560484@qq.com>
2016-07-30 16:51:41 +0800

    Revert "nothing"
    This reverts commit 178a50074585ac3026fb8a29680f4b6e88058aa6.

--- thinkPHP/Application/Home/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-		//数据库配置
-// 		'DB_HOST'=>'localhost',
-// 		'DB_USER'=>'root',
-// 		'DB_PWD'=>'',
-// 		'DB_NAME'=>'music',
-// 		'DB_PREFIX'=>'sq_',
-);
--- thinkPHP/Application/Home/Model/TeacherModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Model;
-use Think\Model;
-class TeacherModel extends Model{
-
-}
-
-?>
